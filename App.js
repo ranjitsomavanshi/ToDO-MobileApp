cb83ab3774d8009472b8849bae2408a5a7100b90
ranjitsomavanshi <ranjitsomavanshi@gmail.com>
2023-04-20 11:12:06 +0530

    input adding functionality added in To Do app

---
+++ App.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Button,
+  TextInput,
+  FlatList,
+} from "react-native";
+
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
+export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+
+
+
+  function addGoalHandler(enteredGoalText) {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      {text: enteredGoalText, id: Math.random().toString()},
+    ]);
+  }
+
+  return (
+    <View style={styles.appContainer}>
+      <GoalInput onAddGoal={addGoalHandler}/>
+      <View style={styles.goalContainer}>
+        <FlatList
+          alwaysBounceVertical={false}
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return <GoalItem text={itemData.item.text}/>;
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+
+  goalContainer: {
+    flex: 5,
+  },
+});
